e78f069dd49cc7e6cbb163e7b009305c1d6f1091
SahanDev00 <Dumidus86@gmail.com>
2024-08-23 12:10:29 +0530

    commited

--- src/Components/Footer2.jsx
+++ src/Components/Footer2.jsx
@@ -11,9 +11,9 @@ const Footer2 = () => {
             <img src={paymentPic} alt="" className='w-[100px] md:w-[200px]' />
             <p className='text-center hidden sm:flex mt-2 font-poppins text-xs md:text-[16px] text-white '>Copyright © <span className='font-semibold'>Exesmart</span> - All rights Reserved</p>
             <div className='flex items-center justify-between gap-2 md:gap-4'>
-                <FaFacebook className='text-white hover:text-red-300 cursor-pointer size-5 md:size-8'/>
-                <FaWhatsapp className='text-white hover:text-red-300 cursor-pointer size-5 md:size-8'/>
-                <FaTwitter className='text-white hover:text-red-300 cursor-pointer size-5 md:size-8'/>
+                <FaFacebook className='text-white hover:text-blue-500 cursor-pointer size-5 md:size-8'/>
+                <FaWhatsapp className='text-white hover:text-green-300 cursor-pointer size-5 md:size-8'/>
+                <FaTwitter className='text-white hover:text-blue-500 cursor-pointer size-5 md:size-8'/>
             </div>
         </div>
         <p className='text-center mx-auto py-3 sm:hidden w-full font-poppins text-xs md:text-[16px] text-white bg-neutral-700'>Copyright © <span className='font-semibold'>Exesmart</span> - All rights Reserved</p>
