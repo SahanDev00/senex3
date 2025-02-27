945b4ad28b4b17dbdafb13324718d3d6f3b6562f
SahanDev00 <Dumidus86@gmail.com>
2025-02-27 11:37:47 +0530

    commited

--- src/Components/Brands.jsx
+++ src/Components/Brands.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Slider from 'react-slick';
-import asus from "../Assets/Images/asus.png";
+import pic from "../Assets/Images/1.png";
+import pic2 from "../Assets/Images/2.png";
+import pic3 from "../Assets/Images/3.png";
+import pic4 from "../Assets/Images/4.png";
+import pic5 from "../Assets/Images/5.png";
+import pic6 from "../Assets/Images/6.png";
+import pic7 from "../Assets/Images/7.png";
 
 const Brands = () => {
   const logos = [
-    { pic: asus },
-    { pic: asus },
-    { pic: asus },
-    { pic: asus },
-    { pic: asus },
-    { pic: asus },
+    { pic: pic },
+    { pic: pic2 },
+    { pic: pic3 },
+    { pic: pic4 },
+    { pic: pic5 },
+    { pic: pic6 },
+    { pic: pic7 },
   ];
 
   const settings = {
@@ -43,8 +50,8 @@ const Brands = () => {
       <div className='w-[95%] md:w-[70%] h-full mb-10 mx-auto'>
         <Slider {...settings}>
           {logos.map((logo, index) => (
-            <div key={index} className='flex justify-center mx-auto w-full '>
-              <img src={logo.pic} className='w-[200px] mx-auto' alt={`Brand ${index}`} />
+            <div key={index} className='flex justify-center mx-auto w-full mt-2 '>
+              <img src={logo.pic} className='w-[200px] h-[100px] object-contain mx-auto' alt={`Brand ${index}`} />
             </div>
           ))}
         </Slider>
